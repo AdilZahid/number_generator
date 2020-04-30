e3ba7999657cd1bf5485de5a819c09d81a00f39b
AdilZahid <Adil.Zahid@ucp.edu.pk>
2020-05-01 04:49:33 +0500

    enable sidekiq and fixed list query

--- app/jobs/sub_job.rb
+++ app/jobs/sub_job.rb
@@ -4,22 +4,22 @@ class SubJob < ApplicationJob
   def perform(u)
     puts "Subjob #{u.name} SubJoB #{DateTime.now}"
     # Snippet For Automatically generate and submission of form
-    # require 'uri'
-    # require 'net/http'
-    # d = CGI.escape(Time.zone.now.strftime("%d/%m/%Y"))
-    # temper = rand(35.6..37.3).round(1)
-    # baseUrl = 'https://temptaking.ado.sg/group/MemberSubmitTemperature?groupCode=8d4f0d34b43753de82e747cb09679631&'
-    # end_url = "date=#{d}&meridies=#{u.meridies}&memberId=#{u.member_id}&temperature=#{temper}&pin=#{u.pin_code}"
-    # final_url = baseUrl.concat(end_url)
-    # url = URI(final_url)
-    # http = Net::HTTP.new(url.host, url.port)
-    # http.use_ssl = true
-    # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    # request = Net::HTTP::Post.new(url)
-    # request["content-type"] = 'application/json'
-    # request["cache-control"] = 'no-cache'
-    # response = http.request(request)
-    # u.update!(submit_status: response.read_body, process_at: Time.zone.now)
+    require 'uri'
+    require 'net/http'
+    d = CGI.escape(Time.zone.now.strftime("%d/%m/%Y"))
+    temper = rand(35.6..37.3).round(1)
+    baseUrl = 'https://temptaking.ado.sg/group/MemberSubmitTemperature?groupCode=8d4f0d34b43753de82e747cb09679631&'
+    end_url = "date=#{d}&meridies=#{u.meridies}&memberId=#{u.member_id}&temperature=#{temper}&pin=#{u.pin_code}"
+    final_url = baseUrl.concat(end_url)
+    url = URI(final_url)
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Post.new(url)
+    request["content-type"] = 'application/json'
+    request["cache-control"] = 'no-cache'
+    response = http.request(request)
+    u.update!(submit_status: response.read_body, process_at: Time.zone.now)
     # Snippet For Automatically generate and submission of form
   end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,7 @@
 class List < ApplicationRecord
-  scope :am_profile_with_ok,      -> { where(process_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day, submit_status: "ok", meridies: "AM") }
-  scope :pm_profile_with_ok,      -> { where(process_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day, submit_status: "ok", meridies: "PM") }
-  scope :today_failed_profile,    -> { where('submit_status != ?', 'ok').where(process_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day) }
+  scope :am_profile_with_ok,      -> { where(process_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day, submit_status: "OK", meridies: "AM") }
+  scope :pm_profile_with_ok,      -> { where(process_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day, submit_status: "OK", meridies: "PM") }
+  scope :today_failed_profile,    -> { where('submit_status != ?', 'OK').where(process_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day) }
   scope :submission_am_profile,   -> { where(meridies: "AM").where.not(process_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day) }
   scope :submission_pm_profile,   -> { where(meridies: "PM").where.not(process_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day) }
 end
